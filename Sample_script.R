be6ef22ae1f363479aa42bc1629caee532737f98
marinebioguy <marinebio_guy@yahoo.com>
2017-11-01 19:40:34 -0500

    Added plot code

--- Sample_script.R
+++ Sample_script.R
@@ -44,3 +44,9 @@ temp_elevation <- read.csv("temp_elevation.csv")
 
 # Commit, pull, push!
 
+(temp.el <-ggplot(temp_elevation, aes(x = Elevation.m, y = Soil.temp.mean)) +
+   geom_point(colour = "#8B4513") +
+   geom_smooth(method = lm, colour = "#8B4513", fill = "#8B4513", alpha = 0.6) +
+   labs(x = "Elevation (m)", y = "Mean soil temperature (°C)") +
+   theme.clean())
+temp.el
